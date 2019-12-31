212058d9764fef734f6ba0393e5ed09f36c25eb6
Vesa Qilerxhiu <44980967+vesaqilerxhiu@users.noreply.github.com>
2019-12-31 16:27:08 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -3,9 +3,18 @@ function fillimi()
     document.getElementById("nowButton").addEventListener(
             "click", printToday, false);
 }
+
 function printToday()
 {
     var today = new Date();
-    window.alert("Date and Time: " + today.toString());
+    var clock = today.getHours();
+    if(clock<12 && clock>5){ 
+    window.alert("Good morning! Date and Time: " + today.toString());
+	}
+	else{
+    window.alert("Good afternoon! Date and Time: " + today.toString());
+	}
+
 }
+
 window.addEventListener("load", fillimi, false);
