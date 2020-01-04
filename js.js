52778cdb75781e72244111ae2435cbbb7806ffef
rinalutolli <52406042+rinalutolli@users.noreply.github.com>
2020-01-04 23:53:11 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -9,12 +9,12 @@ function printToday()
     var today = new Date();
     var clock = today.getHours();
     if(clock<12 && clock>5){ 
-    window.alert("Good morning! Date and Time: " + today.toString());
+    window.alert("Good morning!\nDate and Time: " + today.toString());
 	}
 	else{
-    window.alert("Good afternoon! Date and Time: " + today.toString());
+    window.alert("Good afternoon!\nDate and Time: " + today.toString());
 	}
 
 }
-
 window.addEventListener("load", fillimi, false);
+
